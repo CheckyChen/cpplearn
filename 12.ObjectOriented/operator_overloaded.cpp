1222c77cbd48832677f33f88fba1edde478564e6
checkychen <812961937@qq.com>
2020-09-17 07:29:18 +0800

    左移运算符重载

--- 12.ObjectOriented/operator_overloaded.cpp
+++ 12.ObjectOriented/operator_overloaded.cpp
@@ -5,6 +5,9 @@ using namespace std;
 class Person1 {
 
 public:
+
+
+
 	int m_A;
 	int m_B;
 
@@ -15,7 +18,6 @@ public:
 		tmp.m_B = this->m_B + p2.m_B;
 		return tmp;
 	}*/
-
 };
 
 // 全局函数重载加号运算符
@@ -47,8 +49,39 @@ void plus_test1() {
 }
 
 
+class Person2 {
+	friend ostream& operator<<(ostream& cout, Person2& person);
+public:
+	Person2(int a, int b)
+	{
+		m_A = a;
+		m_B = b;
+	}
+private:
+	int m_A;
+	int m_B;
+};
+
+// 重载左移运算符
+ostream& operator<<(ostream& out_p, Person2& person)
+{
+	out_p << "m_A = " << person.m_A << " m_B = " << person.m_B;
+	return out_p;
+}
+
+void leftmove_test() {
+
+	Person2 p(20, 20);
+
+	cout << p << endl;
+}
+
+
 int main() {
 
 	plus_test1();
+
+	leftmove_test();
+
 	return 1;
 }
